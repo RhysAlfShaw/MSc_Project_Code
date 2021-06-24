da8f28b8825178d0ccb98e73c669576f768388a8
Rhys Shaw <45737158+RhysAlfShaw@users.noreply.github.com>
2021-06-24 11:14:24 +0100

    added GPE and KE and V scaler

--- cluster_inital.py
+++ cluster_inital.py
@@ -1,20 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
-N = 1000             #(number of stars)
+N = 100             #(number of stars)
 Rmax = 1000          #(AU)
                      #Defalut units also include Mass in solar masses.
 X = np.zeros(N)
 Y = np.zeros(N)
 Z = np.zeros(N)
-
-for i in range(0,N):
+i = 0
+while i < N:
     x = np.random.uniform(-Rmax,Rmax)
     y = np.random.uniform(-Rmax,Rmax)
     z = np.random.uniform(-Rmax,Rmax)
     if np.sqrt(x**2+y**2+z**2) < Rmax:
         X[i] = x    
         Y[i] = y
-        Z[i] = z    
+        Z[i] = z
+        i = i + 1    
     
 Vx = np.zeros(N)
 Vy = np.zeros(N)
