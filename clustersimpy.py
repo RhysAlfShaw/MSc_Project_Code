92c7263e193a87f77e10a2f3796938342864797b
Rhys Shaw <45737158+RhysAlfShaw@users.noreply.github.com>
2021-06-27 20:00:02 +0100

    sum

--- clustersimpy.py
+++ clustersimpy.py
@@ -4,20 +4,26 @@ import matplotlib.pyplot as plt
 """
 Constants and Unit conversions
 """
-M_planet = 9.55E-4 #M_sol Jupiter like mass
+
 M_sol = 1.99E30
 G = 6.67E-11
 AU = 1.49E11
 
+
+
 class gen_cluster:
     """
-    This class will create 
+    This class will create a cluster, where options exist for different star distributions
+    different IMFs and for other parameters such as N, radius of clusters sphere,
+    and the virial ratio q.
     """
     def __init__(self,x):
         self.Rmax = x[0]
         self.N = x[1]
         self.q = x[2]
     
+
+
     def Gen_positions(self,distribution):
         X = np.zeros(self.N)
         Y = np.zeros(self.N)
@@ -40,6 +46,8 @@ class gen_cluster:
         else:
             print("INPUT ERROR: Please specify 'Uniform'or ....")
     
+
+
     def Gen_mass(self,IMF):
         if IMF == 'constant':
             Mass = np.zeros(self.N)
@@ -52,6 +60,8 @@ class gen_cluster:
             i = 0 # place holder code!
         else:
             print("INPUT ERROR: Please specify 'constant' or....")
+
+
 
     def Gen_velocities(self):
         Vx = np.zeros(self.N)
@@ -83,6 +93,8 @@ class gen_cluster:
         self.Vy = Vy*a
         self.Vz = Vz*a
 
+
+
     def graph(self):
         fig = plt.figure()
         ax = fig.add_subplot(111,projection='3d')
@@ -90,35 +102,87 @@ class gen_cluster:
         ax.set_box_aspect([1,1,1])
         plt.show()
 
+
+
         
 class get_planet:
     """
     This class will create a planetary systems based on input parameters on a clsuters system.
+    Takes the inputs of theta (to pick the starting position of the orbit), M_star, M_planet, Phi and eccentricity and semi major axis a.
+    Phi should all for the inclination of the orbit to be changed. 
     """
-    def __init__(self):
-        self.i = 0
+    def __init__(self,x):
+        self.theta = x[0]
+        self.M_star = x[1]
+        self.M_planet = x[2]
+        self.phi = x[3]
+        self.a = x[4]
+        self.e = x[5]
+
+    def calculate_params(self):
+        self.c = self.a*self.e
+        self.b = np.sqrt((self.a**2)- (self.c**2))
+        self.R_vec = np.array([self.a*np.cos(self.theta)-self.c, self.b*np.sin(self.theta),0])
+        
+        Vmax = np.sqrt(G*(self.M_star+self.M_planet)*M_sol * ((2)/(np.linalg.norm(self.R_vec)*AU) - (1)/(self.a*AU)))
+        self.V_vec = np.array([Vmax*np.sin(self.theta)*np.cos(self.phi),Vmax*np.cos(self.theta)*np.cos(self.phi),Vmax*np.sin(self.phi)])
+    
+    def graph(self,dimension):
+        t = np.linspace(0,2*np.pi, 200)
+        x = -self.c + self.a*np.cos(t)
+        y = self.b*np.sin(t)
+        
+        if dimension == '2D':
+
+            plt.figure(figsize=[5*self.a,5*self.b]) #figsize changes to appropriate deimensions.
+            plt.plot(x,y,linestyle='--')
+            plt.scatter(0,0,label='Star')
+            plt.scatter(self.R_vec[0],self.R_vec[1],label='Planet')
+            plt.legend()
+            plt.show()
+
+        if dimension == '3D':
+
+            fig = plt.figure()
+            ax = fig.add_subplot(111,projection='3d')
+            ax.scatter(0,0,0,label='star',s=40)
+            ax.plot(x,y,0,linestyle='--')
+            ax.scatter(self.R_vec[0],self.R_vec[1],self.R_vec[2],label='Planet')
+            ax.set_box_aspect([1*self.a,1*self.b,1]) #figsize changes to appropriate dimentions
+            plt.legend()
+            plt.show()
+
+        
+
 
 
 
 class measure:
+
     """
-    allows for measurements of a system.
+    allows for measurements of a system. Giving only R_vec and V_vec. This will be important
+    when measuring the properties of otbits over time.
+    
     """
+
     def __init__(self,data):
         self.M_star = data[0]
         self.M_planet = data[1]
         self.R_vec = data[2]
         self.V_vec = data[3]
     
+
     def calculate_a(self):
         mu = self.M_star*M_sol*M_sol*self.M_planet/(self.M_planet*M_sol+self.M_star*M_sol)
         Eb = abs(0.5*mu*np.linalg.norm(self.V_vec)**2  - (G*self.M_star*M_sol*self.M_planet*M_sol)/(np.linalg.norm(self.R_vec)*AU))
         a_calc = (G * self.M_star*M_sol * self.M_planet*M_sol) / (2*Eb*AU)
         return a_calc #units of AU
     
+
     def calculate_e(self,a):
         e = np.sqrt((1-(np.linalg.norm(self.R_vec))/(abs(a)) )**2 + (np.dot(self.R_vec,self.V_vec)**2)/(abs(a)*G*(self.M_planet+self.M_star)*M_sol))
         return e
+
     
     def graph(self,dimension,a,e):
         c = a*e #from the definition of eccentricity.
@@ -126,14 +190,18 @@ class measure:
         t = np.linspace(0,2*np.pi, 200)
         x = -c + a*np.cos(t)
         y = b*np.sin(t)
+
         if dimension == '2D':
+
             plt.figure(figsize=[5*a,5*b]) #figsize changes to appropriate deimensions.
             plt.plot(x,y,linestyle='--')
             plt.scatter(0,0,label='Star')
             plt.scatter(self.R_vec[0],self.R_vec[1],label='Planet')
             plt.legend()
             plt.show()
+
         if dimension == '3D':
+
             fig = plt.figure()
             ax = fig.add_subplot(111,projection='3d')
             ax.scatter(0,0,0,label='star',s=40)
@@ -142,5 +210,3 @@ class measure:
             ax.set_box_aspect([1*a,1*b,1]) #figsize changes to appropriate dimentions
             plt.legend()
             plt.show()
-        else:
-            print("INPUT ERROR: Please specify '2D' or '3D' only!")
